1c364589173ff90bd7ad4bd3b149c357edf56e7f
SYPIAC <Reks888@hotmail.com>
2016-09-03 12:56:00 +0300

    find if groups connect

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -23,4 +23,29 @@ class GamesController < ApplicationController
     render json: {status: :ok}
   end
 
+  def connects? group_1, group_2, board
+    for x in 0..board.length-1
+      for y in 0..board.length-1
+        if board[y][x] == group_1 then
+          if neighbours(x,y, board).any?{|group| group==group_2} then
+            return true
+          end
+        end
+      end
+    end
+    return false
+  end
+
+  def neighbours x, y, board
+    neighbours = [] << board[y+1][x] if x < board[0].length
+    neighbours << board[y][x+1] if y < board.length
+    neighbours << board[y-1][x] if y > 0
+    neighbours << board[y][x-1] if x > 0
+    return neighbours
+  end
+  #color array = [[-1,-1...]] if no color
+  #group and color are numbers
+  def change_color group, color, colorArray
+    #colorArray
+  end
 end
